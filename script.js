9dcfa9b547ce57e19b7957df4739f4f3a50d054d
Berkley Horan <horans11@gmail.com>
2018-11-04 19:53:15 -0700

    Complete

--- script.js
+++ script.js
@@ -1,28 +1,60 @@
 
 
-function solve(){
-
-    var stack = [];
-    this.solvestring = function(value){
+function solve(value){ 
         value = value.split(" ");
+        var stack = [];
 
         for(var i = 0; i < value.length; i++){
-            if(value[i].isNumeric()){
-                value.push(value[i]);
-            }
-            else{
-                var val1 = value.pop();
-                var val2 = value.pop();
-
-
-
-
-
+            if(!isNaN(value[i])){
+                stack.push(value[i]);
+            } else{
+                var val1 = stack.pop();
+                var val2 = stack.pop();
+                    if (value[i] === "+"){
+                        stack.push(parseInt(val2) + parseInt(val1));
+                    }
+                    else if (value[i] === "-"){
+                        stack.push(parseInt(val1) - parseInt(val2));
+                    }
+                    else if (value[i] === "*"){
+                        stack.push(parseInt(val1) * parseInt(val2));
+                    }
+                    else if (value[i] === "/"){
+                        stack.push(parseInt(val2) / parseInt(val1));
+                    }
+                    else if (value[i] === "^"){
+                        stack.push(Math.pow(parseInt(val1), parseInt(val2)));
+                    }
 
             }
         }
+        
+
+        if(stack.length < 0){
+            return "error";
+        }
+        else{
+            return stack.pop()
+        }
+
          
 
+}
+
+    //bug fix from stack overflow, doesnt work but now NAN does...
+    String.prototype.isNumeric = function() {
+        return !isNaN(parseFloat(this)) && isFinite(this);
     }
 
-}
+    function input(){
+
+        var equation = document.getElementById("input").value;
+
+        //success!!
+        if(equation.length > 1){
+        alert(solve(equation))
+        } else{
+        alert("INSERT EQUATION")
+        }
+    }
+    
